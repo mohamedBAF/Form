d8724b1f12bfcb8361c53de5f8074626c1540c2b
mohamedBAF <mohamed.benabedelfattah@esprit.tn>
2020-10-23 15:43:59 +0200

    Updates

---
+++ precache-manifest.acd099d0fb1b6842e638bce3c1d8ce73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf5fd09055bbf140ff74bf38fa9afa3",
+    "url": "/Form/index.html"
+  },
+  {
+    "revision": "5bc3397269752246afb9",
+    "url": "/Form/static/css/main.696ef1eb.chunk.css"
+  },
+  {
+    "revision": "e3e639bb592de6f1264b",
+    "url": "/Form/static/js/2.b8bdbf25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Form/static/js/2.b8bdbf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc3397269752246afb9",
+    "url": "/Form/static/js/main.9da3ab85.chunk.js"
+  },
+  {
+    "revision": "7ba1abebbaf199f56366",
+    "url": "/Form/static/js/runtime-main.001676b4.js"
+  }
+]);
